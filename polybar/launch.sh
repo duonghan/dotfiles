ec8d56d777811682bd598a12224ec55f6b92a8cb
duonghan <20155296@student.hust.edu.vn>
2018-10-03 10:12:42 +0700

    add polybar config and update i3 config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/ sh
+#!/usr/bin/env zsh
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >//dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar i3 
